a5d730771bb7a471f5dae8a77cd00eb48b38af90
GhostGamer146 <146650592+GhostGamer146@users.noreply.github.com>
2026-03-10 17:55:29 +0530

    Assembly Definition

---
+++ Assets/10-3-26/Mukul/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Noob");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
