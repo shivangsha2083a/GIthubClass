d810a002f2ff870cf28f6cda3f040545482ecf4d
PratyushGambiraopet12 <pratyush9190@gmail.com>
2026-03-11 17:07:01 +0530

    main

---
+++ Assets/11-03-26/Pratyush/PlayerContoller.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("Anything");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
