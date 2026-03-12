8beba05743f636cc4422c813769ad73125ac2c9e
Praveen Kumar <160683601+PraveenTechWizard@users.noreply.github.com>
2026-03-12 17:27:45 +0530

    Scriptable Object

---
+++ Assets/12-03-26/Praveen/PraveenData.cs
@@ -0,0 +1,9 @@
+using Unity.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PraveenData", menuName = "Scriptable Objects/PraveenData")]
+public class PraveenData : ScriptableObject
+{
+    public string Name = "Praveen";
+    public int Number = 00000;
+}
