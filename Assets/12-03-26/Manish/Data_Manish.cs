215400a1c97955c57f2d33d6c3ecf0899b8eee0e
ManishChetla <chetlamanish24@gmail.com>
2026-03-12 17:17:22 +0530

    Manish Chetla
    Scriptable Object Done

---
+++ Assets/12-03-26/Manish/Data_Manish.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Data", menuName = "Data_1/Data_2")]
+
+public class Data_Manish : ScriptableObject
+{
+
+}
---
+++ Assets/12-03-26/Manish/PlayerData_Manish.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Manish", menuName ="Manish_Data")] 
+public class PlayerData_Manish : ScriptableObject
+{
+    public string Name;
+    public int rollNo;
+}
