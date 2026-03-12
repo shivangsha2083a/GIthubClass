2da59a0c2bb9b3d0cda67f9fe477e7cbebc0bd77
Harshith Varma <varmazenitsu04@gmail.com>
2026-03-12 17:31:28 +0530

    Harshith

---
+++ Assets/12-03-26/Harshith/Playerdata2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Playerdata", menuName = "Playerdata/Playerdata2")]
+public class Playerdata2 : Playerdata
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
