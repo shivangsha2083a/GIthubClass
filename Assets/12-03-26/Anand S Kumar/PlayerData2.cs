ef0286e02ee5a64b0b6be5b04b5f4f296de6b63c
Anand S Kumar <134936237+mjhkjtytjk@users.noreply.github.com>
2026-03-13 17:19:14 +0530

    Anand S Kumar

---
+++ Assets/12-03-26/Anand S Kumar/PlayerData2.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData2", menuName = "Player_Data/PlayerData2")]
+public class PlayerData2 : PlayerData
+{
+    public PlayerData playerData;
+    
+}
