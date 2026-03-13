119d372a8ae986d32e960c8940b413e01231b476
PratyushGambiraopet12 <pratyush9190@gmail.com>
2026-03-13 17:16:14 +0530

    pratyush

---
+++ Assets/12-03-26/Pratyush/PlayerData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public string name;
+
+    public int roll;
+}
---
+++ Assets/12-03-26/Pratyush/PlayerData2.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerData2", menuName = "PlayerData/PlayerData2")]
+
+public class PlayerData2 : PlayerData
+{
+    public PlayerData playerdata;
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
