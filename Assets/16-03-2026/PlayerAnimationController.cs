04d751da6e5aeadb186052f062b06e5f7e5397a2
PratyushGambiraopet12 <pratyush9190@gmail.com>
2026-03-16 18:14:38 +0530

    pratyush

--- Assets/16-03-2026/PlayerAnimationController.cs
+++ Assets/16-03-2026/PlayerAnimationController.cs
@@ -17,7 +17,7 @@ public class PlayerAnimationController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        animator.SetFloat("Horizontal", horizontalInput);
-        animator.SetFloat("Vertical", verticalInput);
+        animator.SetFloat("InputX", horizontalInput);
+        animator.SetFloat("InputY", verticalInput);
     }
 }
