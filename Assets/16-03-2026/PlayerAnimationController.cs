4f18690594900b62e128cff887db0147053add41
ManishChetla <chetlamanish24@gmail.com>
2026-03-16 17:47:01 +0530

    Animation Script Updated

---
+++ Assets/16-03-2026/PlayerAnimationController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", horizontalInput);
+        animator.SetFloat("Vertical", verticalInput);
+    }
+}
