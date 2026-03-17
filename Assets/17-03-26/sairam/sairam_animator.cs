be6a4fe312bcaa95e8bd85a6af1d8e00086fb487
sairamshanker970-dotcom <sairamshanker970@gmail.com>
2026-03-17 17:29:58 +0530

    slime rabbit prefab created

---
+++ Assets/17-03-26/sairam/sairam_animator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class sairam_animator : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            //do the transitiont to first animation
+        }
+    }
+}
